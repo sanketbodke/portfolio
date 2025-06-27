b0375654ab7573f7cd546f55352a4d613e7b3e31
Sanket Bodke <84568517+sanketbodke@users.noreply.github.com>
2025-06-27 22:27:09 +0530

    project setup

---
+++ next-portfolio/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = React.memo(function Footer() {
+  return (
+    <footer>
+      <p>Footer</p>
+    </footer>
+  );
+});
+
+export default Footer;
---
+++ next-portfolio/components/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = React.memo(function Navbar() {
+  return (
+    <nav>
+      <p>Navbar</p>
+    </nav>
+  );
+});
+
+export default Navbar;
